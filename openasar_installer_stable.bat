fb87236717e4dd6fcc5c9529165693700f730490
JohnzinN <157098820+TeamXOS@users.noreply.github.com>
2025-10-03 19:14:59 -0300

    openasar
    openasar

---
+++ openasar_installer_stable.bat
@@ -0,0 +1,37 @@
+@echo off
+echo Closing Discord... (wait around 5 seconds)
+
+C:\Windows\System32\TASKKILL.exe /f /im Discord.exe > nul 2> nul
+C:\Windows\System32\TASKKILL.exe /f /im Discord.exe > nul 2> nul
+C:\Windows\System32\TASKKILL.exe /f /im Discord.exe > nul 2> nul
+
+C:\Windows\System32\TIMEOUT.exe /t 5 /nobreak > nul 2> nul
+
+echo Installing OpenAsar... (ignore any blue output flashes)
+copy /y "%localappdata%\Discord\app-1.0.9210\resources\app.asar" "%localappdata%\Discord\app-1.0.9210\resources\app.asar.backup" > nul 2> nul
+if exist "%localappdata%\Discord\app-1.0.9210\resources\_app.asar" copy /y "%localappdata%\Discord\app-1.0.9210\resources\_app.asar" "%localappdata%\Discord\app-1.0.9210\resources\app.asar.backup" > nul 2> nul
+if exist "%localappdata%\Discord\app-1.0.9210\resources\app.asar.orig" copy /y "%localappdata%\Discord\app-1.0.9210\resources\app.asar.orig" "%localappdata%\Discord\app-1.0.9210\resources\app.asar.backup" > nul 2> nul
+
+powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9210\resources\app.asar\"" > nul 2> nul
+
+if exist "%localappdata%\Discord\app-1.0.9059\resources\app.asar" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9059\resources\app.asar\"" > nul 2> nul
+if exist "%localappdata%\Discord\app-1.0.9058\resources\app.asar" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9058\resources\app.asar\"" > nul 2> nul
+if exist "%localappdata%\Discord\app-1.0.9057\resources\app.asar" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9057\resources\app.asar\"" > nul 2> nul
+
+if exist "%localappdata%\Discord\app-1.0.9209\resources\app.asar" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9209\resources\app.asar\"" > nul 2> nul
+if exist "%localappdata%\Discord\app-1.0.9208\resources\app.asar" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9208\resources\app.asar\"" > nul 2> nul
+if exist "%localappdata%\Discord\app-1.0.9207\resources\app.asar" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9207\resources\app.asar\"" > nul 2> nul
+
+if exist "%localappdata%\Discord\app-1.0.9211\resources\app.asar" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9211\resources\app.asar\"" > nul 2> nul
+if exist "%localappdata%\Discord\app-1.0.9212\resources\app.asar" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9212\resources\app.asar\"" > nul 2> nul
+if exist "%localappdata%\Discord\app-1.0.9213\resources\app.asar" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9213\resources\app.asar\"" > nul 2> nul
+
+if exist "%localappdata%\Discord\app-1.0.9210\resources\_app.asar" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9210\resources\_app.asar\"" > nul 2> nul
+if exist "%localappdata%\Discord\app-1.0.9210\resources\app.asar.orig" powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile \"$Env:LOCALAPPDATA\Discord\app-1.0.9210\resources\app.asar.orig\"" > nul 2> nul
+
+echo Opening Discord...
+start "" "%localappdata%\Discord\Update.exe" --processStart Discord.exe > nul 2> nul
+
+C:\Windows\System32\TIMEOUT.exe /t 1 /nobreak > nul 2> nul
+
+exit
