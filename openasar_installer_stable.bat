8c692a731bf1ec772f104dca0d5ab467fde45170
JohnzinN <157098820+TeamXOS@users.noreply.github.com>
2025-10-03 19:41:57 -0300

    OpenAsar

--- openasar_installer_stable.bat
+++ openasar_installer_stable.bat
@@ -1,12 +1,4 @@
 @echo off
-echo Closing Discord... (wait around 5 seconds)
-
-C:\Windows\System32\TASKKILL.exe /f /im Discord.exe > nul 2> nul
-C:\Windows\System32\TASKKILL.exe /f /im Discord.exe > nul 2> nul
-C:\Windows\System32\TASKKILL.exe /f /im Discord.exe > nul 2> nul
-
-C:\Windows\System32\TIMEOUT.exe /t 5 /nobreak > nul 2> nul
-
 echo Installing OpenAsar... (ignore any blue output flashes)
 copy /y "%localappdata%\Discord\app-1.0.9210\resources\app.asar" "%localappdata%\Discord\app-1.0.9210\resources\app.asar.backup" > nul 2> nul
 if exist "%localappdata%\Discord\app-1.0.9210\resources\_app.asar" copy /y "%localappdata%\Discord\app-1.0.9210\resources\_app.asar" "%localappdata%\Discord\app-1.0.9210\resources\app.asar.backup" > nul 2> nul
